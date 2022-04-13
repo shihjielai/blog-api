4a0d4c7e1fa34378f821a33c00f081157f9b5af8
Jay <shihjielai@gmail.com>
2022-04-13 15:52:19 +0800

    新增 Role entity 和 CustomUserDetailsService

---
+++ src/main/java/com/springboot/blog/entity/Role.java
@@ -0,0 +1,21 @@
+package com.springboot.blog.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
--- src/main/java/com/springboot/blog/entity/User.java
+++ src/main/java/com/springboot/blog/entity/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "users", uniqueConstraints = {
@@ -24,4 +25,11 @@ public class User {
     private String userName;
     private String email;
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
 }
